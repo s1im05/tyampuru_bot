e25d942d4e1550a9eaeacc77d46fe97b8665f031
Vadim Suleymanov <v.sul.frontend@gmail.com>
2019-06-25 15:44:37 +0200

    Simple key-value storage to restrict extra polling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-const poll_up = '❤';
-const poll_down = '✖';
+const icon_poll_up = '❤';
+const icon_poll_down = '✖';
 const icon_globe = '🌍';
 const last_id_file = 'last_id';
 const chat_id = '@tyampuru';
@@ -10,13 +10,21 @@ const link_path = 'https://tyampuru.ru/post/';
 const TOKEN = process.env.TELEGRAM_TOKEN || '';
 const POST_DELAY = process.env.POST_DELAY || 60;
 
+const ACTION_POLL = 'poll';
+const ACTION_POLL_UP = 'poll_up';
+const ACTION_POLL_DOWN = 'poll_down';
+
 const TelegramBot = require('node-telegram-bot-api');
+const Keyv = require('keyv');
 const request = require('request');
 const fs = require('fs');
+
+const keyv_ttl = 60 * 60 * 1000; // 1 hour
 const options = {
     polling: true
 };
 const bot = new TelegramBot(TOKEN, options);
+const keyv = new Keyv();
 
 const getPost = (postId) => {
     return new Promise((resolve, reject) => {
@@ -46,6 +54,35 @@ const getPost = (postId) => {
     });
 };
 
+const doPoll = async (data, msg) => {
+
+    const postKey = `post_${data.postId}`;
+    const userVoteKey = `${postKey}_user_${msg.chat.id}`;
+
+    let postVoteData = await keyv.get(postKey);
+    if (!postVoteData) {
+        const reply = msg.reply_markup.inline_keyboard;
+
+        postVoteData = {
+            voteUp: +reply[0][0].text.substr(2),
+            voteDown: +reply[0][1].text.substr(2),
+        };
+    }
+
+    const hasUserVote = await keyv.get(userVoteKey);
+    if (!hasUserVote) {
+        if (data.vote === ACTION_POLL_UP) {
+            postVoteData.voteUp++;
+        } else {
+            postVoteData.voteDown++;
+        }
+        await keyv.set(userVoteKey, true, keyv_ttl);
+    }
+    await keyv.set(postKey, postVoteData, keyv_ttl);
+
+    return postVoteData;
+};
+
 const sendNextPost = () => {
 
     let lastId = 0;
@@ -65,8 +102,8 @@ const sendNextPost = () => {
         return bot.sendPhoto(chat_id, data.image, {
             reply_markup: {
                 inline_keyboard: [[
-                    {text: `${poll_up} 0`, callback_data: JSON.stringify({'action': 'poll', 'vote': 'up', 'postId': data.id})},
-                    {text: `${poll_down} 0`, callback_data: JSON.stringify({'action': 'poll', 'vote': 'down', 'postId': data.id})}
+                    {text: `${icon_poll_up} 0`, callback_data: JSON.stringify({'action': ACTION_POLL, 'vote': ACTION_POLL_UP, 'postId': data.id})},
+                    {text: `${icon_poll_down} 0`, callback_data: JSON.stringify({'action': ACTION_POLL, 'vote': ACTION_POLL_DOWN, 'postId': data.id})}
                 ], [
                     {text: `${icon_globe} ссылка`, url: `${link_path}${data.id}`}
                 ]]
@@ -80,13 +117,15 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
     const data = JSON.parse(callbackQuery.data);
     const msg = callbackQuery.message;
 
-    if (data.action === 'poll') {
+    if (data.action === ACTION_POLL) {
         (async () => {
+
+            const pollData = await doPoll(data, msg);
+
             try {
                 const reply = msg.reply_markup.inline_keyboard;
-                const index = data.vote === 'up' ? 0 : 1;
-                const vote = +reply[0][index].text.substr(2);
-                reply[0][index].text = reply[0][index].text.substr(0, 2) + (vote+1);
+                reply[0][0].text = `${icon_poll_up} ${pollData.voteUp}`;
+                reply[0][1].text = `${icon_poll_down} ${pollData.voteDown}`;
 
                 await bot.editMessageReplyMarkup({
                     inline_keyboard: [...reply]
